446495a0c8114ce6ce7c97bce0bf5b1c666b7934
Benjamin Pisano <benjamin.pisano@icloud.com>
2017-11-27 14:58:33 +0100

    solve

--- sources/solve.c
+++ sources/solve.c
@@ -6,7 +6,7 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/23 12:11:12 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2017/11/24 13:58:31 by htaillef    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/11/27 14:58:22 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -97,15 +97,71 @@ void	remove_t(t_map *map, t_tetri *tetri, int x, int y)
 	}
 }
 
+int		contact(t_map *map, int x, int y)
+{
+	int		t_x;
+	int		t_y;
+	int		abs;
+
+	t_y = y - 1;
+	while (t_y <= y + 1)
+	{
+		t_x = x - 1;
+		while (t_x <= x + 1)
+		{
+			abs = x - y < 0 ? -(x - y) : x - y;
+			if (abs == 1 && t_x - 1 >= 0 && t_y - 1 >= 0)
+			{
+				if (map->map[t_y][t_x] == '.')
+					return (1);
+			}
+			t_x++;
+		}
+		t_y++;
+	}
+	return (0);
+}
+
+int		isolated_blocks(t_map *map, t_tetri *tetri, int x, int y)
+{
+	int		o_x;
+	int		o_y;
+	int		t_x;
+	int		t_y;
+	int		count;
+
+	o_x = x;
+	o_y = y;
+	t_y = 0;
+	count = 0;
+	while (y < o_y + tetri->height)
+	{
+		x = o_x;
+		t_x = 0;
+		while (x < o_x + tetri->width)
+		{
+			if (!contact(map, x, y) && map->map[y][x] == '.')
+				count++;
+			x++;
+			t_x++;
+		}
+		y++;
+		t_y++;
+	}
+	return (count);
+}
+
 int		try(t_map *map)
 {
 	int				x;
 	int				y;
+	int				isolated;
 	t_list			*current_t;
 
 	if (!(map->todo))
 		return (1);
-	if (!(current_t = ft_lstpopi(&(map->todo), 0)))
+	printf("\n%d, %d\n", ft_lstlen(map->todo) * 4, map->area);
+	if (!(current_t = ft_lstpopi(&(map->todo), 0)) || ft_lstlen(map->todo) * 4 > map->area)
 		return (0);
 	y = -1;
 	while (++y < map->size)
@@ -115,10 +171,18 @@ int		try(t_map *map)
 		{
 			if (put(map, (t_tetri *)current_t->content, x, y))
 			{
+				//print_map(map->map);
+				//sleep(1);
+				isolated = isolated_blocks(map, (t_tetri *)(current_t->content), x, y);
+				map->area -= isolated + 4;
+				//printf("ok\n");
 				if (try(map))
 					return (1);
 				else
+				{
+					map->area += isolated + 4;
 					remove_t(map, (t_tetri *)(current_t->content), x, y);
+				}
 			}
 		}
 	}
@@ -128,12 +192,9 @@ int		try(t_map *map)
 
 void	solve(t_map *map)
 {
-	t_list	initial_todo;
-
-	initial_todo = *map->todo;
 	while (!try(map))
 	{
-		map->todo = &initial_todo;
+		//printf("resize\n");
 		map->size++;
 		free_dual_strings(map->map);
 		map->map = char_map(map->size);
