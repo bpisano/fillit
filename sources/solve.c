e550445038da4cac8c64281dcda4217f426da70f
Benjamin Pisano <benjamin.pisano@icloud.com>
2017-11-27 20:11:58 +0100

    Opti algo

--- sources/solve.c
+++ sources/solve.c
@@ -6,13 +6,13 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/23 12:11:12 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2017/11/27 14:58:22 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/11/27 20:11:03 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
-
+#include <stdio.h>
 int		can_put(t_map *map, t_tetri *tetri, int x, int y)
 {
 	int		o_x;
@@ -97,58 +97,45 @@ void	remove_t(t_map *map, t_tetri *tetri, int x, int y)
 	}
 }
 
-int		contact(t_map *map, int x, int y)
+int		contact(t_map *map, int x, int y, int deep)
 {
-	int		t_x;
-	int		t_y;
-	int		abs;
-
-	t_y = y - 1;
-	while (t_y <= y + 1)
-	{
-		t_x = x - 1;
-		while (t_x <= x + 1)
-		{
-			abs = x - y < 0 ? -(x - y) : x - y;
-			if (abs == 1 && t_x - 1 >= 0 && t_y - 1 >= 0)
-			{
-				if (map->map[t_y][t_x] == '.')
-					return (1);
-			}
-			t_x++;
-		}
-		t_y++;
-	}
-	return (0);
+	if (deep > 4)
+		return (0);
+	if (x > map->size - 1 || y > map->size - 1 || map->map[y][x] != '.')
+		return (1);
+	return (1 + (contact(map, x, y + 1, deep + 1) || contact(map, x + 1, y, deep + 1)));
 }
 
 int		isolated_blocks(t_map *map, t_tetri *tetri, int x, int y)
 {
 	int		o_x;
 	int		o_y;
-	int		t_x;
-	int		t_y;
 	int		count;
 
 	o_x = x;
 	o_y = y;
-	t_y = 0;
 	count = 0;
-	while (y < o_y + tetri->height)
+	while (y - o_y < tetri->height)
 	{
 		x = o_x;
-		t_x = 0;
-		while (x < o_x + tetri->width)
+		while (x - o_x < tetri->width)
 		{
-			if (!contact(map, x, y) && map->map[y][x] == '.')
-				count++;
+			
+			if (count > 3)
+				count = 0;
+			if ((x - 1 < 0 && y - 1 < 0) || (y - 1 < 0 && map->map[y][x - 1] != '.') || (x - 1 < 0 && map->map[y - 1][x] != '.'))
+			{
+				if (map->map[y][x] == '.')
+				{	
+					count += contact(map, x, y, 0);
+					//printf("Count : %d, %d, %d\n", count, x, y);
+				}
+			}
 			x++;
-			t_x++;
 		}
 		y++;
-		t_y++;
 	}
-	return (count);
+	return (count > 3 ? 0 : count);
 }
 
 int		try(t_map *map)
@@ -159,10 +146,11 @@ int		try(t_map *map)
 	t_list			*current_t;
 
 	if (!(map->todo))
-		return (1);
-	printf("\n%d, %d\n", ft_lstlen(map->todo) * 4, map->area);
-	if (!(current_t = ft_lstpopi(&(map->todo), 0)) || ft_lstlen(map->todo) * 4 > map->area)
+		return (1);	
+	if (ft_lstlen(map->todo) * 4 > map->area || !(current_t = ft_lstpopi(&(map->todo), 0)))
+	{
 		return (0);
+	}
 	y = -1;
 	while (++y < map->size)
 	{
@@ -171,21 +159,25 @@ int		try(t_map *map)
 		{
 			if (put(map, (t_tetri *)current_t->content, x, y))
 			{
+				//printf("\n\n");
+				//printf("MAP\n");
 				//print_map(map->map);
-				//sleep(1);
 				isolated = isolated_blocks(map, (t_tetri *)(current_t->content), x, y);
-				map->area -= isolated + 4;
-				//printf("ok\n");
+				map->area -= (isolated + 4);		
+				//printf("Isolated : %d\n", isolated);
+				//printf("Area : %d\n", map->area);
+				//printf("Estimated todo area : %d\n", ft_lstlen(map->todo) * 4);
+				//sleep(1);
 				if (try(map))
 					return (1);
 				else
 				{
 					map->area += isolated + 4;
 					remove_t(map, (t_tetri *)(current_t->content), x, y);
-				}
+				}	
 			}
 		}
-	}
+	}	
 	ft_lstadd(&(map->todo), current_t);
 	return (0);
 }
@@ -194,8 +186,8 @@ void	solve(t_map *map)
 {
 	while (!try(map))
 	{
-		//printf("resize\n");
 		map->size++;
+		map->area = map->size * map->size;
 		free_dual_strings(map->map);
 		map->map = char_map(map->size);
 	}
